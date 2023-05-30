bec51e487265da65dd2a13140f0f824557e3bd16
canknbr <cankanbur1@gmail.com>
2023-05-30 16:31:38 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Can Kanbur on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
